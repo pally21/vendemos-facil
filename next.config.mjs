7547474ac8452f8ad7006a185bd9b67e1c02c9d6
pally21 <joha.gajardo@duocuc.cl>
2026-05-09 18:40:08 -0400

    fix: next.config.mjs sintaxis javascript

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,5 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  // Evita que Next detecte un root incorrecto cuando hay lockfiles fuera del proyecto.
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   outputFileTracingRoot: process.cwd(),
 };
-
 export default nextConfig;
